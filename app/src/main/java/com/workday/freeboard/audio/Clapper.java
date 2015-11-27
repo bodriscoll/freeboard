9324c3543359f6acc56b5b6225bb4ecb8d017823
marion.herkert <marion.herkert@workday.com>
2015-11-27 12:15:13 +0000

    Initial commit with all the shits

---
+++ app/src/main/java/com/workday/freeboard/audio/Clapper.java
@@ -0,0 +1,142 @@
+package com.workday.freeboard.audio;
+
+import android.media.MediaRecorder;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class Clapper
+{
+
+//    private static final String TAG = "Clapper";
+//
+//    private static final long DEFAULT_CLIP_TIME = 1000;
+//    private long clipTime = DEFAULT_CLIP_TIME;
+//    private AmplitudeClipListener clipListener;
+//
+//    private boolean continueRecording;
+//
+//    /**
+//     * how much louder is required to hear a clap 10000, 18000, 25000 are good
+//     * values
+//     */
+//    private int amplitudeThreshold;
+//
+//    /**
+//     * requires a little of noise by the user to trigger, background noise may
+//     * trigger it
+//     */
+//    public static final int AMPLITUDE_DIFF_LOW = 10000;
+//    public static final int AMPLITUDE_DIFF_MED = 18000;
+//    /**
+//     * requires a lot of noise by the user to trigger. background noise isn't
+//     * likely to be this loud
+//     */
+//    public static final int AMPLITUDE_DIFF_HIGH = 25000;
+//
+//    private static final int DEFAULT_AMPLITUDE_DIFF = AMPLITUDE_DIFF_MED;
+//
+//    private MediaRecorder recorder;
+//
+//    private String tmpAudioFile;
+//
+//    public Clapper() throws IOException
+//    {
+//        this(DEFAULT_CLIP_TIME, "/tmp.3gp", DEFAULT_AMPLITUDE_DIFF, null, null);
+//    }
+//
+//    public Clapper(long snipTime, String tmpAudioFile,
+//                   int amplitudeDifference, Context context, AmplitudeClipListener clipListener)
+//            throws IOException
+//    {
+//        this.clipTime = snipTime;
+//        this.clipListener = clipListener;
+//        this.amplitudeThreshold = amplitudeDifference;
+//        this.tmpAudioFile = tmpAudioFile;
+//    }
+//
+//    public boolean recordClap()
+//    {
+//        Log.d(TAG, "record clap");
+//        boolean clapDetected = false;
+//
+//        try
+//        {
+//            recorder = AudioUtil.prepareRecorder(tmpAudioFile);
+//        }
+//        catch (IOException io)
+//        {
+//            Log.d(TAG, "failed to prepare recorder ", io);
+//            throw new RecordingFailedException("failed to create recorder", io);
+//        }
+//
+//        recorder.start();
+//        int startAmplitude = recorder.getMaxAmplitude();
+//        Log.d(TAG, "starting amplitude: " + startAmplitude);
+//
+//        do
+//        {
+//            Log.d(TAG, "waiting while recording...");
+//            waitSome();
+//            int finishAmplitude = recorder.getMaxAmplitude();
+//            if (clipListener != null)
+//            {
+//                clipListener.heard(finishAmplitude);
+//            }
+//
+//            int ampDifference = finishAmplitude - startAmplitude;
+//            if (ampDifference >= amplitudeThreshold)
+//            {
+//                Log.d(TAG, "heard a clap!");
+//                clapDetected = true;
+//            }
+//            Log.d(TAG, "finishing amplitude: " + finishAmplitude + " diff: "
+//                    + ampDifference);
+//        } while (continueRecording || !clapDetected);
+//
+//        Log.d(TAG, "stopped recording");
+//        done();
+//
+//        return clapDetected;
+//    }
+//
+//    private void waitSome()
+//    {
+//        try
+//        {
+//            // wait a while
+//            Thread.sleep(clipTime);
+//        } catch (InterruptedException e)
+//        {
+//            Log.d(TAG, "interrupted");
+//        }
+//    }
+//
+//    /**
+//     * need to call this when completely done with recording
+//     */
+//    public void done()
+//    {
+//        Log.d(TAG, "stop recording");
+//        if (recorder != null)
+//        {
+//            if (isRecording())
+//            {
+//                stopRecording();
+//            }
+//            //now stop the media player
+//            recorder.stop();
+//            recorder.release();
+//        }
+//    }
+//
+//    public boolean isRecording()
+//    {
+//        return continueRecording;
+//    }
+//
+//    public void stopRecording()
+//    {
+//        continueRecording = false;
+//    }
+}
